e04c193eb4873ddd12a54c0df0dde36481b84f6d
noMugop <nomugopradio@gmail.com>
2022-04-12 18:43:27 +0600

    side bar

--- app/src/main/java/com/example/myfilms/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myfilms/presentation/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.myfilms.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,9 +14,11 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.myfilms.R
 import com.example.myfilms.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
@@ -21,33 +27,64 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        init()
-        bottomBarVisibility()
+        navController = findNavController(R.id.main_container)
+        bottomBarInit()
+        sideBarInit()
+        setVisibility()
     }
 
-    private fun bottomBarVisibility() {
+    private fun setVisibility() {
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             when (destination.id) {
                 R.id.filmsFragment -> {
                     binding.bottomNavigationView.visibility = View.VISIBLE
+                    binding.sideNavigation.visibility = View.VISIBLE
                 }
                 R.id.favoritesFragment -> {
                     binding.bottomNavigationView.visibility = View.VISIBLE
+                    binding.sideNavigation.visibility = View.VISIBLE
                 }
                 R.id.detailsFragment -> {
                     binding.bottomNavigationView.visibility = View.GONE
+                    binding.sideNavigation.visibility = View.GONE
                 }
                 R.id.loginFragment -> {
                     binding.bottomNavigationView.visibility = View.GONE
+                    binding.sideNavigation.visibility = View.GONE
                 }
             }
         }
     }
 
-    private fun init() {
-        navController = findNavController(R.id.main_container)
+    private fun sideBarInit() {
+
+        binding.sideNavigation.setNavigationItemSelectedListener(this)
+
+        binding.topAppbar.setNavigationOnClickListener { object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                binding.drawerMainActivity.openDrawer(GravityCompat.START)
+            }
+        } }
+//        binding.sideNavigation.setupWithNavController(navController)
+    }
+
+    private fun bottomBarInit() {
         binding.bottomNavigationView.labelVisibilityMode =
             NavigationBarView.LABEL_VISIBILITY_LABELED
         binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.side_films -> Toast.makeText(applicationContext, "Home", Toast.LENGTH_SHORT).show()
+            R.id.side_favorite -> Toast.makeText(applicationContext, "Favorites", Toast.LENGTH_SHORT).show()
+            R.id.settings -> Toast.makeText(applicationContext, "Settings", Toast.LENGTH_SHORT).show()
+            R.id.about -> Toast.makeText(applicationContext, "About", Toast.LENGTH_SHORT).show()
+            R.id.exit -> Toast.makeText(applicationContext, "Exit", Toast.LENGTH_SHORT).show()
+            R.id.share -> Toast.makeText(applicationContext, "Share", Toast.LENGTH_SHORT).show()
+            R.id.rate_us -> Toast.makeText(applicationContext, "Rate_us", Toast.LENGTH_SHORT).show()
+            else -> Toast.makeText(applicationContext, "Nothing Clicked", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
