d55a602ede9b673209a3537e0a4d625e0d85deee
noMugop <nomugopradio@gmail.com>
2022-05-12 14:18:42 +0600

    EditText added

--- app/src/main/java/com/example/myfilms/presentation/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/myfilms/presentation/fragments/settings/SettingsFragment.kt
@@ -110,6 +110,7 @@ class SettingsFragment : Fragment() {
         })
 
         binding.etSurname.addTextChangedListener(object : TextWatcher {
+
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
 
